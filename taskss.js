1180497cda6b03e32f44345dccd7ddaae817802f
sachin123git <109585774+sachin123git@users.noreply.github.com>
2023-06-24 00:48:45 +0530

    Add files via upload

---
+++ taskss.js
@@ -0,0 +1,89 @@
+// let data = {
+
+// }
+// data.name = "john"
+// console.log(data);
+// data.surname = "smith"
+// console.log(data);
+// data.name = "pete"
+// console.log(data);
+// delete data.name
+// console.log(data)
+
+
+// let person1 = { name: "John", age: 25 };
+// let person2 = { name: "pete", age: 30 };
+// let person3 = { name: "mary", age: 35 };
+
+// let people = [person1, person2, person3];
+
+// let printName = (person) => {
+//   console.log(person.name);
+// };
+// let printage = (person) => {
+//   console.log(person.age);
+// };
+
+// people.map(printName);
+// people.map(printage)
+
+
+// let array = ["appale","pear","orange"]
+// let shopingCart = array;
+// console.log(shopingCart)
+// shopingCart.push("banana")
+// console.log(shopingCart)
+// console.log(shopingCart.length)
+
+
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 },
+// ];
+
+// console.log(usersMapped[0])
+// console.log(usersMapped[0].fullName)
+
+
+// let salaries = {
+// "John": 100,
+// "Pete": 300,
+// "Mary": 250
+// };
+
+// function sumSalaries(){
+//      let sum = 0 ;
+//      for(let salary of Object.values(salaries)){
+//         sum = sum + salary
+//      }
+//      return sum
+// }
+// console.log(sumSalaries(salaries))
+
+
+// let user = {
+//     name:'sachin',
+//     age: 23
+// }
+
+// let {name,age,isAdmin = false}=user
+
+// console.log(name)
+// console.log(age)
+// console.log(isAdmin)
+
+
+// let user = {
+//     name: 'sachin',
+//     age:23,
+//     study: "IT"
+// }
+// console.log(user)
+
+// let uesrJSON = JSON.stringify(user)
+// console.log(uesrJSON)
+// let backuser = JSON.parse(uesrJSON)
+// console.log(backuser)
+
+
